86e4264c3f1f5b13b411fddcd87bb49e84df041e
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-06-04 18:28:38 +0200

    ckey spec rewrite

--- spec/ckey_spec.rb
+++ spec/ckey_spec.rb
@@ -6,61 +6,40 @@ require 'helpers/ckey_helper'
 
 describe CKey do
 
-  describe "delegate event to the EventDispatcher" do
+  it "invokes main api when using test methods" do
+    CKey.stub(:grab_keyboard).with(subject)
+    CKey.should_receive(:grab_keyboard).with(subject)
+    CKey.test(subject).puts('a')
+  end
 
-    # The general test is ready (below) but I have been yet 
-    # unable to # make it pass. It requires multiple actions 
-    # at once, # i.e.: grabbing the keyboard and simulating 
-    # events.
-    # Ruby stops managing threads whenever a C extension is
-    # being executed. Forking the block in question led me
-    # to memory issues, the same happened when I created a
-    # thread with C's pthread library. TODO: resolve!
-    #
-    # it "delegates key_press events to the EventDispatcher" do
-    #   event_dispatcher = EventDispatcher.new
-    #   CKey.grab_keyboard(event_dispatcher)
-    #   event_dispatcher.stub(:key_press).with('a'){ true }
-    #   event_dispatcher.should_receive(:key_press).with('a')
-    #   CKey.simulate_keypress_supermode('a')
-    # end
+  describe "delegates events" do
 
     before do
-      @event_dispatcher = EventDispatcher.new
-      @event_dispatcher.stub(:key_press).with('a')
-      @event_dispatcher.stub(:key_release).with('a')
-      @event_dispatcher.stub(:key_press).with('b')
-      @event_dispatcher.stub(:key_release).with('b')
-      @event_dispatcher.stub(:wait).with(nil)
+      @e = EventDispatcher.new
     end
 
-    it "invokes main api when using test methods" do
-      CKey.stub(:grab_keyboard).with(@event_dispatcher)
-      CKey.should_receive(:grab_keyboard).with(@event_dispatcher)
-      CKey.test(@event_dispatcher).puts('a')
-    end
-
-    it "delegates key-related events to the EventDispatcher" do
-      @event_dispatcher.should_receive(:key_press).with('a').once
-      @event_dispatcher.should_receive(:key_release).with('a').once
-      CKey.test(@event_dispatcher).puts('a')
-    end
-
-    it "informs about user's idleness" do
-      @event_dispatcher.should_receive(:wait).with().twice
-      CKey.test(@event_dispatcher).wait(2)
+    it "delegates single-key events" do
+      @e.should_receive(:key_press).with('a').once
+      @e.should_receive(:key_release).with('a').once
+      CKey.test(@e).puts('a')
     end
 
     it "delegates all events" do
-      @event_dispatcher.should_receive(:key_press).with('a').exactly(3).times
-      @event_dispatcher.should_receive(:key_release).with('a').exactly(3).times
-      @event_dispatcher.should_receive(:key_press).with('b').once
-      @event_dispatcher.should_receive(:key_release).with('b').once
-      CKey.test(@event_dispatcher).puts('a')
-      CKey.test(@event_dispatcher).puts('a')
-      CKey.test(@event_dispatcher).puts('b')
-      CKey.test(@event_dispatcher).puts('a')
+      @e.should_receive(:key_press).with('a').exactly(3).times
+      @e.should_receive(:key_release).with('a').exactly(3).times
+      @e.should_receive(:key_press).with('b').once
+      @e.should_receive(:key_release).with('b').once
+      CKey.test(@e).puts('a')
+      CKey.test(@e).puts('a')
+      CKey.test(@e).puts('b')
+      CKey.test(@e).puts('a')
     end
+
+    it "informs about user's idleness" do
+      @e.should_receive(:wait).with().twice
+      CKey.test(@e).wait(2)
+    end
+
   end
 
 end
