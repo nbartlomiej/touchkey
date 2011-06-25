fdd6b19b3aa334defba839df0abc961152368600
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-06-25 19:55:18 +0200

    user preferences spec

---
+++ spec/user_preferences_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'UserPreferences' do
+  it "loads a file from ~/.touchkey.conf" do
+    pending
+  end
+
+  it "creates a file at ~/.touchkey.conf if such file does not exist" do
+    pending
+  end
+
+  it "imports configuratino from ~/.touchkey.conf file" do
+    pending
+  end
+end
