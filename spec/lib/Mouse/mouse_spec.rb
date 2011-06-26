27fe0d2b9741dd4c59fbd1b3cf99cfe2362afb67
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-06-26 22:37:23 +0200

    improved Mouse spec, removed set_mouse_rel

--- spec/lib/Mouse/mouse_spec.rb
+++ spec/lib/Mouse/mouse_spec.rb
@@ -7,12 +7,12 @@ describe Touchkey::Mouse do
 
   before(:all) do
     # register the mouse's initial position
-    @mouse_position = [Touchkey::Mouse.get_mouse_x, Touchkey::Mouse.get_mouse_y]
+    # @mouse_position = [Touchkey::Mouse.get_mouse_x, Touchkey::Mouse.get_mouse_y]
   end
 
   after(:all) do
     # restore the mouse's initial position
-    Touchkey::Mouse.set_mouse_abs(*@mouse_position)
+    # Touchkey::Mouse.set_mouse_abs(*@mouse_position)
   end
 
   describe ".set_mouse_abs" do
@@ -22,15 +22,10 @@ describe Touchkey::Mouse do
       Touchkey::Mouse.get_mouse_y.should == 20
     end
   end
-  describe ".set_mouse_rel" do
-    it "sets mouse pointer relatively to previous position" do
-      Touchkey::Mouse.set_mouse_abs(10,20)
-      Touchkey::Mouse.set_mouse_rel(5,3)
-      Touchkey::Mouse.get_mouse_x.should == 15
-      Touchkey::Mouse.get_mouse_y.should == 23
-    end
-  end
   describe ".get_mouse_x" do
+    pending
+  end
+  describe ".left_click" do
     pending
   end
 end
