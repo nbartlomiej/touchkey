7b88f27d9a92478940ea375c06de4c7c9a2d28d5
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-10-01 18:04:38 +0200

    improved mouse spec

--- spec/lib/Mouse/mouse_spec.rb
+++ spec/lib/Mouse/mouse_spec.rb
@@ -7,12 +7,12 @@ describe Touchkey::Mouse do
 
   before(:all) do
     # register the mouse's initial position
-    # @mouse_position = [Touchkey::Mouse.get_mouse_x, Touchkey::Mouse.get_mouse_y]
+    @mouse_position = [Touchkey::Mouse.get_mouse_x, Touchkey::Mouse.get_mouse_y]
   end
 
   after(:all) do
     # restore the mouse's initial position
-    # Touchkey::Mouse.set_mouse_abs(*@mouse_position)
+    Touchkey::Mouse.set_mouse_abs(*@mouse_position)
   end
 
   describe ".set_mouse_abs" do
@@ -23,7 +23,15 @@ describe Touchkey::Mouse do
     end
   end
   describe ".get_mouse_x" do
-    pending
+    it "does not raise an error" do
+      expect { subject.get_mouse_x }.to_not raise_error
+    end
+    it "is an integer" do
+      subject.get_mouse_x.to_i.should == subject.get_mouse_x
+    end
+    it "is non-negative number" do
+      subject.get_mouse_x.should be >= 0
+    end
   end
   describe ".left_click" do
     pending
