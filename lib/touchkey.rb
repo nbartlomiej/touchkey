2a2d58fb05ab8e01fc63e664bf4f9c9587a2abac
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-10-14 21:22:04 +0200

    temporary fix for the mouse spec failures

--- lib/touchkey.rb
+++ lib/touchkey.rb
@@ -5,8 +5,10 @@ require 'touchkey/event_dispatchers/simple_dispatcher'
 require 'touchkey/event_dispatchers/aggregator'
 require 'touchkey/portable'
 
-Touchkey::Portable::require_native 'touchkey/Key/Key'
+# The order of these requires matters; should be: Mouse after Key. If Key is
+# loaded before Mouse, the mouse spec fails. TODO: investivate.
 Touchkey::Portable::require_native 'touchkey/Mouse/Mouse'
+Touchkey::Portable::require_native 'touchkey/Key/Key'
 
 module Touchkey
 
